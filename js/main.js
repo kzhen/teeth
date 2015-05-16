68d566fdf4e52d2b53bb4d2ebc1dc1d342b26d3f
KenR <dev@kenpire.com>
2015-05-16 14:49:36 +0100

    Added teeth

--- js/main.js
+++ js/main.js
@@ -1,1 +1,86 @@
+var colorDecay = "#cb3594";
+var colorFilling = "#659b41";
+var colorYellow = "#ffcf33";
+var colorBrown = "#986928";
 
+
+
+
+var curColor = colorDecay;
+var clickColor = new Array();
+
+var context = document.getElementById('canvas').getContext("2d");
+
+$('#canvas').mousedown(function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+		
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+$('#canvas').mousemove(function(e){
+  if(paint){
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+$('#canvas').mouseup(function(e){
+  paint = false;
+});
+
+$('#canvas').mouseleave(function(e){
+  paint = false;
+});
+
+
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+  clickColor.push(curColor);
+}
+
+function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+  context.strokeStyle = "#df4b26";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+			
+  for(var i=0; i < clickX.length; i++) {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+	 context.strokeStyle = clickColor[i];
+     context.stroke();
+  }
+}
+
+
+$("#btnDecay").click(function() {
+	curColor = colorDecay;
+});
+
+$("#btnFilling").click(function() {
+	curColor = colorFilling;
+});
+
+$(".tooth").click(function() {
+	//alert(this.src);
+	$("#canvas").css("background", "url("+this.src+")");
+});
